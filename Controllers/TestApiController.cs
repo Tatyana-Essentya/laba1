36a7ea9e62f7cb7d61e8e8e6ed965453a3b1d2b5
TSPU\student <tatiana.mikhailowwa2000@gmail.com>
2023-04-15 10:34:14 +0300

    lab 1

---
+++ Controllers/TestApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace TspuWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        [HttpGet("{count}")] 
+        public ActionResult<int[]> Get([FromRoute] int count)
+        {
+            int[] array = new int[count];
+            Random rand = new Random();
+            for(int x = 0; x<array.Length; x++)
+            {
+                array[x] = rand.Next();
+            }
+            return Ok(array);
+        }
+    }
+}
